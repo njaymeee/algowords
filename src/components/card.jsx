adc0d28ad681f0c4a629af4719557584c5da5ace
Njayyymeee <njayzar.r1982@gmail.com>
2025-03-19 22:33:34 +0800

    Update card.jsx

--- src/components/card.jsx
+++ src/components/card.jsx
@@ -5,9 +5,9 @@ import Button from './button.jsx';
 function Card(props) {
     return (
         <div className="max-w-sm rounded overflow-hidden shadow-md transform transition-transform duration-300 hover:scale-105">
-            <img src={props.image} alt="Card" className="w-full" />
+           {/* <img src={import.meta.env.BASE_URL + props.Image} alt="Card" className="w-full" /> */}
             <div className="px-6 py-4">
-                <arcticle className="mb-0.5">
+                <arcticle className="mb-0.5 h-50">
                 <h2 className="font-bold text-xl mb-2">{props.header}</h2>
                 <p className="text-gray-700 text-base">{props.paragraph}</p>
                 </arcticle>
